6e807dc8fa965f7d55c6d29c417da42c64d933bd
66Meg20 <66meg.dj2066@gmail.com>
2023-02-22 23:44:33 -0600

    added the up button functionality

---
+++ index.js
@@ -0,0 +1,9 @@
+let counter = 0;
+function count() {
+  counter++;
+  document.getElementById("number").innerHTML = counter;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("up-btn").onclick = count;
+});
