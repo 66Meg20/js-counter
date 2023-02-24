1975ceed7a085f10e3c24edfe91fd9d1f71e7886
66Meg20 <66meg.dj2066@gmail.com>
2023-02-24 00:49:16 -0600

    added the functionality to both buttons

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-let counter = 0;
-function count() {
-  counter++;
-  document.getElementById("number").innerHTML = counter;
-}
+//let counter = 0;
+//function count() {
+//  counter++;
+// document.getElementById("number").innerHTML = counter;
+//}
 
-document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("up-btn").onclick = count;
+//document.addEventListener("DOMContentLoaded", function () {
+// document.getElementById("up-btn").onclick = count;
+//});
+
+let add = document.querySelector("#up-btn");
+let subract = document.querySelector("#down-btn");
+
+add.addEventListener("click", function () {
+  let output = document.querySelector("#number");
+  let result = Number(output.innerHTML) + 1;
+
+  output.innerText = result;
+});
+
+subract.addEventListener("click", function () {
+  let output = document.querySelector("#number");
+  let result = Number(output.innerText) - 1;
+
+  output.innerText = result;
 });
