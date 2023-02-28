968b68e3dbe10e04b7b2bed9c6ce4d9c28d68173
66Meg20 <66meg.dj2066@gmail.com>
2023-02-27 21:49:38 -0600

    added some styling

--- index.js
+++ index.js
@@ -10,20 +10,3 @@ function downBtn() {
   --value;
   document.querySelector("#number").innerHTML = value;
 }
-
-//let add = document.querySelector("#up-btn");
-//let subract = document.querySelector("#down-btn");
-
-//add.addEventListener("click", function () {
-//let output = document.querySelector("#number");
-//let result = Number(output.innerHTML) + 1;
-
-// output.innerHTML = result;
-//});
-
-//subract.addEventListener("click", function () {
-//let output = document.querySelector("#number");
-//let result = Number(output.innerText) - 1;
-
-//output.innerHTML = result;
-//});
